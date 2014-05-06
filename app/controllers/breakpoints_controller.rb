14c2f691c4933b08b4003c28e6623303726237a7
Ilia Reingold <ilia.reingold@gmail.com>
2014-05-06 15:26:43 -0400

    Added create action to breakpoints controller

--- app/controllers/breakpoints_controller.rb
+++ app/controllers/breakpoints_controller.rb
@@ -1,2 +1,14 @@
 class BreakpointsController < ApplicationController
+
+	def create
+		@project = Project.find(params[:project_id])
+		@breakpoint = @project.breakpoints.new(breakpoint_params)
+
+		if @breakpoint.save
+			redirect_to @project
+		else
+			# empty for now
+		end
+	end
+
 end
