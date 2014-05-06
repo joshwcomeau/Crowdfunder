33becb5213deb3d61f5317bad8e8034ea79bcea9
Ilia Reingold <ilia.reingold@gmail.com>
2014-05-06 13:25:45 -0400

    made the projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Crowdfunder::Application.routes.draw do
+  root "projects#index"
+ resources :projects 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
