3de59f6addfbfaa13830df8d7029136f792fabad
Ilia Reingold <ilia.reingold@gmail.com>
2014-05-06 13:33:02 -0400

    updated projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,45 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def new
+    @project = Project.new
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end 
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to root_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
