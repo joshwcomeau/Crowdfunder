5f247bf89acee6562cb714a27904f1c30ef928c7
Ilia Reingold <ilia.reingold@gmail.com>
2014-05-07 16:55:48 -0400

    pledge model made

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507192013) do
+ActiveRecord::Schema.define(version: 20140507202239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20140507192013) do
     t.integer  "user_id"
     t.decimal  "amount"
     t.text     "breakpoint_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pledges", force: true do |t|
+    t.decimal  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
