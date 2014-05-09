81df49d4e68ffebf7d74eab9208e15b9d024742d
shmems11 <shmems11@gmail.com>
2014-05-09 16:26:00 -0400

    image works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,7 +23,6 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    binding.pry
     @project = Project.new(project_params)
     @project.user_id = current_user.id if current_user
     if @project.save
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [800, 800]
+  process :resize_to_limit => [400, 400]
   #
   # def scale(width, height)
   #   # do something
@@ -38,9 +38,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
